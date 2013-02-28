ef5e5bd53cdb10c0682177726a1cb4d09c503bc1
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2013-02-28 17:07:40 +0100

    Delete ruby features
    Woot ... Ruby ... Beurk ...

--- mode-mappings.el
+++ mode-mappings.el
@@ -31,17 +31,6 @@
 (add-to-list 'auto-mode-alist '("\\.jsp$" . crappy-jsp-mode))
 (add-to-list 'auto-mode-alist '("\\.jspf$" . crappy-jsp-mode))
 
-;; Ruby
-(autoload 'rhtml-mode "rhtml-mode")
-(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.watchr$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("capfile" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.erb$" . rhtml-mode))
-
 ;; Clojure
 (autoload 'clojure-mode "clojure-mode")
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
