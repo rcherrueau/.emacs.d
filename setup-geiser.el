7be462979e065adaa4a431f0bc67d5fc0f515092
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2013-07-24 14:47:05 +0200

    Typo
    Use symbole instead of variable name in eq.
    Add last parentheses missing.

--- setup-geiser.el
+++ setup-geiser.el
@@ -5,9 +5,9 @@
 (require 'geiser)
 
 ;; Set path of Racket REPL
-(when (eq geiser-racket-collects nil)
+(when (eq 'geiser-racket-collects nil)
   (when is-mac
-    (setq geiser-racket-binary "/Applications/Racket v5.3.4/bin/racket"))
+    (setq geiser-racket-binary "/Applications/Racket v5.3.4/bin/racket")))
 
 ;; Set racket as the default REPL
 (setq geiser-active-implementations '(racket))
