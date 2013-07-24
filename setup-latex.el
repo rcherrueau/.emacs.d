ec734db11b761d06248b05c4b9e379107dda4d25
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2013-07-24 13:43:19 +0200

    Add hook on bibtex-clean-entry
    Call bibtex-reformat after clean entry
    Call validate after clean entry

--- setup-latex.el
+++ setup-latex.el
@@ -5,4 +5,12 @@
 (add-hook 'latex-mode-hook 'auto-fill-mode)
 (setq-default fill-column 70)
 
+
+;; Launch reformat and validate after
+;; the clean entry on bibentry
+(add-hook 'bibtex-clean-entry-hook
+          (lambda ()
+            (bibtex-reformat)
+            (bibtex-validate)))
+
 (provide 'setup-latex)
