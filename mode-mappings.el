2f3533c0ebeb20e3239e2b5e796fe6c2584450cd
Ronan-Alexandre Cherrueau <RonanCherrueau@gmail.com>
2014-04-24 17:41:48 +0200

    Remove Crappy JSP Mode

--- mode-mappings.el
+++ mode-mappings.el
@@ -22,11 +22,6 @@
             (require 'rename-sgml-tag)
             (define-key sgml-mode-map (kbd "C-c C-r") 'rename-sgml-tag)))
 
-;; JSP
-(autoload 'crappy-jsp-mode "crappy-jsp-mode")
-(add-to-list 'auto-mode-alist '("\\.jsp$" . crappy-jsp-mode))
-(add-to-list 'auto-mode-alist '("\\.jspf$" . crappy-jsp-mode))
-
 ;; Clojure
 (autoload 'clojure-mode "clojure-mode")
 (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
